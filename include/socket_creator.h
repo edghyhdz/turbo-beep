1d6985513e968a6582c7aa7fd0cdffa86bf4d8ca
edghyhdz <edgar.hdz@hotmail.com>
2021-07-23 07:42:42 +0200

    Moved file to include directory

---
+++ include/socket_creator.h
@@ -0,0 +1,42 @@
+/**
+ * Socket class declaration
+ */
+
+#ifndef SOCKET_CREATOR
+#define SOCKET_CREATOR
+#include <arpa/inet.h>
+#include <memory>
+#include <netdb.h>
+#include <unistd.h>
+
+class Socket {
+public:
+  Socket(char *&ipAddress, char *&portNum);
+  // Socket(const Socket &) = delete;
+  // ~Socket();
+
+  void bindToPort();
+  void connectToPeer(std::uint16_t const peerPort, std::string const peerIp);
+  void connectToServer(std::uint16_t const serverPort,
+                       std::string const serverIp);
+  void close();
+
+  std::string ipAddress() const { return _myIpAddress; }
+  std::uint16_t port() const { return _myPort; }
+
+private:
+  addrinfo _hints, *_p;
+  struct sockaddr_in _myAddr, peerAddr;
+  void _setIpAddress();
+  void _connectToServer();
+  void _listenToServer(); 
+  void _connectToPeer(std::uint16_t const peerPort, std::string const peerIp);
+  
+  int _sockFD; 
+  int _addrInfo; 
+  std::string _myIpAddress;
+  std::uint16_t _myPort;
+  bool _connectionOpen;
+};
+
+#endif
