177d872a8ed889c8b2f79325357522d69322e9d7
edghyhdz <edgar.hdz@hotmail.com>
2021-07-24 11:17:30 +0200

    Added couple class member attributes - _peerName and _t (thread) to be used by _listenToServer() method

--- include/socket_creator.h
+++ include/socket_creator.h
@@ -8,19 +8,15 @@
 #include <memory>
 #include <netdb.h>
 #include <unistd.h>
+#include <thread>
 
 class Socket {
 public:
-  Socket(char *&ipAddress, char *&portNum);
-  // Socket(const Socket &) = delete;
-  // ~Socket();
-
+  Socket(char *&ipAddress, char *&portNum, std::string userName, std::string peerName);
+  ~Socket(); 
   void bindToPort();
   void connectToPeer(std::uint16_t const peerPort, std::string const peerIp);
-  void connectToServer(std::uint16_t const serverPort,
-                       std::string const serverIp);
   void close();
-
   std::string ipAddress() const { return _myIpAddress; }
   std::uint16_t port() const { return _myPort; }
 
@@ -33,11 +29,14 @@ private:
   void _listenToServer(); 
   void _connectToPeer(std::uint16_t const peerPort, std::string const peerIp);
   
+  std::string _userName; 
   int _sockFD; 
   int _addrInfo; 
   std::string _myIpAddress;
   std::uint16_t _myPort;
   bool _connectionOpen;
+  std::thread _t; 
+  std::string _peerName; 
 };
 
 #endif
