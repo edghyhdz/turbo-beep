a21673af4d91c9ee4e3d4d8508775b5e81125603
edghyhdz <edgar.hdz@hotmail.com>
2021-07-25 10:29:32 +0200

    Added _mutex and _cond private member attributes

--- include/socket_creator.h
+++ include/socket_creator.h
@@ -5,16 +5,19 @@
 #ifndef SOCKET_CREATOR
 #define SOCKET_CREATOR
 #include <arpa/inet.h>
+#include <condition_variable>
 #include <memory>
+#include <mutex>
 #include <netdb.h>
-#include <unistd.h>
 #include <thread>
+#include <unistd.h>
 
 class Socket {
 public:
-  Socket(char *&ipAddress, char *&portNum, std::string userName, std::string peerName);
-  ~Socket(); 
-  void bindToPort();
+  Socket(char *&ipAddress, char *&portNum, std::string userName,
+         std::string peerName);
+  ~Socket();
+  void connectToServer();
   void connectToPeer();
   void close();
   std::string ipAddress() const { return _myIpAddress; }
@@ -24,22 +27,24 @@ private:
   addrinfo _hints, *_p;
   struct sockaddr_in _myAddr, _peerAddr;
   void _setIpAddress();
-  void _setPort(); 
-  void _bindToPort(); 
+  void _setPort();
+  void _bindToPort();
   void _connectToServer();
-  void _listenToServer(); 
-  
-  std::string _userName; 
-  int _sockFD; 
-  int _connFD; 
-  int _addrInfo; 
+  void _listenToServer();
+
+  std::string _userName;
+  int _sockFD;
+  int _connFD;
+  int _addrInfo;
   std::string _myIpAddress;
   std::uint16_t _myPort;
   bool _connectionOpen;
-  std::thread _t; 
-  std::string _peerName; 
-  std::uint16_t _peerPort; 
-  std::string _peerIpAddress; 
+  std::thread _t;
+  std::string _peerName;
+  std::uint16_t _peerPort;
+  std::string _peerIpAddress;
+  std::mutex _mutex;
+  std::condition_variable _cond; 
 };
 
 #endif
