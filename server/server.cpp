41462e63b9b0e579b55abe160e2dcc5dcdb1dfc5
edghyhdz <edgar.hdz@hotmail.com>
2021-07-25 15:34:42 +0200

    Changed _updatePeerInfo(), _findPeer(), _removePeer(), _readyToP2P() and _findPeerInformation() as class member methods

--- server/server.cpp
+++ server/server.cpp
@@ -15,131 +15,8 @@
 #include <unistd.h>
 #include <vector>
 
-/**
- * Updates peer information with other's peer information (ipAddress and port).
- *
- * @param[in, out] uD `userDescriptor` connecting's peer information
- * @param[in, out] user current users' name
- * @param[in] peer Client's peer to connect to
- */
-void updatePeerInfo(std::map<std::string, userInfo> *uD,
-                    std::string const &user, std::string const &peer) {
-  uD->at(user).canConnect = true;
-  uD->at(user).peerInfo.ipAddress = uD->at(peer).ipAddress;
-  uD->at(user).peerInfo.port = uD->at(peer).port;;
-}
-
-/**
- * Finds whether if peer's peer is already connected.
- * Peer that connects first will be consired the client, and thus
- * `isClient = true` will be assigned.
- *
- * @param[in, out] userDescriptor connecting's peer information
- * @param[in, out] user current users' name
- * @param[in] peer Client's peer to connect to
- */
-void findPeer(std::map<std::string, userInfo> *userDescriptor,
-              std::string const &user, std::string const &peer) {
-  if (userDescriptor->find(peer) == userDescriptor->end()) {
-    userDescriptor->at(user).isClient = true;
-  } else {
-    // Both are now connected
-    userDescriptor->at(user).isClient = false;
-    updatePeerInfo(userDescriptor, user, peer);
-    updatePeerInfo(userDescriptor, peer, user);
-  }
-}
-
-/**
- * Remove peer from `userDescriptor` once peer has disconnected from server
- *
- * @param[in, out] uD `userDescriptor` map containing user's information
- * @param[in] socket disconnected user's socket
- */
-void removePeer(std::map<std::string, userInfo> *uD, int const &socket) {
-  if (!uD->empty()) {
-    for (auto key_val : *uD) {
-      if (socket == key_val.second.socket) {
-        std::cout << "Erased: " << key_val.second.name << std::endl;
-        uD->erase(key_val.second.name);
-        break;
-      }
-    }
-  }
-}
-
-/**
- * Checks if both peers have `canConnect`=true, this only happens once both
- * peers have successfully connected to the server
- *
- * @param[in, out] uD `userDescriptor` map containing user's information
- * @param[in] socket disconnected user's socket
- */
-void readyToP2P(std::map<std::string, userInfo> const &uD, int const &socket) {
-
-  for (auto key_val : uD) {
-    if (socket == key_val.second.socket) {
-      if (key_val.second.canConnect) {
-        // Send message to both peers
-        std::string peerName = key_val.second.peerInfo.name;
-        int peerSocket = uD.at(peerName).socket;
-
-        std::string ipAddressPeer =
-            uD.at(peerName).peerInfo.ipAddress + ":" +
-            std::to_string(uD.at(peerName).peerInfo.port);
-
-        std::string ipAddressCl =
-            key_val.second.peerInfo.ipAddress + ":" +
-            std::to_string(key_val.second.peerInfo.port);
-
-        // Send peer information to other peer
-        send(peerSocket, ipAddressPeer.c_str(), ipAddressPeer.length() + 1, 0);
-        send(socket, ipAddressCl.c_str(), ipAddressCl.length() + 1, 0);
-        break;
-      }
-    }
-  }
-}
-
-void findPeerInformation(std::string &buffer, std::map<std::string, userInfo> *uD, int sock){
-  std::string delimiter = "::";
-
-  if (buffer.find(delimiter) != std::string::npos) {
-    size_t pos = 0;
-    std::vector<std::string> params;
-
-    while ((pos = buffer.find(delimiter)) != std::string::npos) {
-      params.push_back(buffer.substr(0, pos));
-      buffer.erase(0, pos + delimiter.length());
-    }
-
-    params.push_back(buffer);
-
-    if (uD->find(params[2]) == uD->end()) {
-      // If username not found -> add it to map
-      userInfo newUser;
-      newUser.ipAddress = params[0];
-      newUser.port = stoi(params[1]); 
-      newUser.name = params[2];
-      newUser.socket = sock;
-      newUser.peerInfo.name = params[3];
-      uD->insert(std::make_pair(params[2], newUser));
-      findPeer(uD, params[2], params[3]);
-    }
-
-    for (auto const &key_val : *uD) {
-      std::cout << key_val.first
-                << " has address: " << key_val.second.ipAddress << ":"
-                << key_val.second.port
-                << " with socket: " << key_val.second.socket
-                << " and peer to connect to: " << key_val.second.peerInfo.name
-                << std::endl;
-    }
-  }
-}
-
 /*****************************************************************************
- * Class member methods start here
+ * Server class member definitions
  */
 
 Server::Server() { this->initServer(); }
@@ -175,9 +52,124 @@ int Server::initServer() {
   FD_SET(_listening, &_master);
 }
 
-void Server::runServer() {
-  std::map<std::string, userInfo> userDescriptor;
+/**
+ * Updates peer information (`_userDescriptor`) with other's peer information (ipAddress and port).
+ *
+ * @param[in, out] user current users' name
+ * @param[in] peer Client's peer to connect to
+ */
+void Server::_updatePeerInfo(std::string const &user, std::string const &peer){
+  _userDescriptor.at(user).canConnect = true;
+  _userDescriptor.at(user).peerInfo.ipAddress = _userDescriptor.at(peer).ipAddress;
+  _userDescriptor.at(user).peerInfo.port = _userDescriptor.at(peer).port;;
 
+}
+
+/**
+ * Finds whether if peer's peer is already connected.
+ * Peer that connects first will be consired the client, and thus
+ * `isClient = true` will be assigned.
+ *
+ * @param[in, out] user current users' name
+ * @param[in] peer Client's peer to connect to
+ */
+void Server::_findPeer(std::string const &user, std::string const &peer) {
+  if (_userDescriptor.find(peer) == _userDescriptor.end()) {
+    _userDescriptor.at(user).isClient = true;
+  } else {
+    // Both are now connected
+    _userDescriptor.at(user).isClient = false;
+    _updatePeerInfo(user, peer);
+    _updatePeerInfo(peer, user);
+  }
+}
+
+/**
+ * Remove peer from `_userDescriptor` once peer has disconnected from server
+ *
+ * @param[in] socket disconnected user's socket
+ */
+void Server::_removePeer(int const &socket) {
+  for (auto key_val : _userDescriptor) {
+    if (socket == key_val.second.socket) {
+      std::cout << "Erased: " << key_val.second.name << std::endl;
+      _userDescriptor.erase(key_val.second.name);
+      break;
+    }
+  }
+}
+
+/**
+ * Checks if both peers have `canConnect`=true, this only happens once both
+ * peers have successfully connected to the server
+ *
+ * @param[in] socket disconnected user's socket
+ */
+void Server::_readyToP2P(int const &socket) {
+
+  for (auto key_val : _userDescriptor) {
+    if (socket == key_val.second.socket) {
+      if (key_val.second.canConnect) {
+
+        // Send message to both peers
+        std::string peerName = key_val.second.peerInfo.name;
+        int peerSocket = _userDescriptor.at(peerName).socket;
+
+        std::string ipAddressPeer =
+            _userDescriptor.at(peerName).peerInfo.ipAddress + ":" +
+            std::to_string(_userDescriptor.at(peerName).peerInfo.port);
+
+        std::string ipAddressCl =
+            key_val.second.peerInfo.ipAddress + ":" +
+            std::to_string(key_val.second.peerInfo.port);
+
+        // Send peer information to other peer
+        send(peerSocket, ipAddressPeer.c_str(), ipAddressPeer.length() + 1, 0);
+        send(socket, ipAddressCl.c_str(), ipAddressCl.length() + 1, 0);
+        break;
+      }
+    }
+  }
+}
+
+void Server::_findPeerInformation(std::string &buffer, int sock){
+  std::string delimiter = "::";
+
+  if (buffer.find(delimiter) != std::string::npos) {
+    size_t pos = 0;
+    std::vector<std::string> params;
+
+    while ((pos = buffer.find(delimiter)) != std::string::npos) {
+      params.push_back(buffer.substr(0, pos));
+      buffer.erase(0, pos + delimiter.length());
+    }
+    
+    params.push_back(buffer);
+    
+    if (_userDescriptor.find(params[2]) == _userDescriptor.end()) {
+      // If username not found -> add it to map
+      userInfo newUser;
+      newUser.ipAddress = params[0];
+      newUser.port = stoi(params[1]); 
+      newUser.name = params[2];
+      newUser.socket = sock;
+      newUser.peerInfo.name = params[3];
+      _userDescriptor.insert(std::make_pair(params[2], newUser));
+      _findPeer(params[2], params[3]);
+    }
+
+    for (auto const &key_val : _userDescriptor) {
+      std::cout << key_val.first
+                << " has address: " << key_val.second.ipAddress << ":"
+                << key_val.second.port
+                << " with socket: " << key_val.second.socket
+                << " and peer to connect to: " << key_val.second.peerInfo.name
+                << std::endl;
+    }
+  }
+}
+
+void Server::runServer() {
   while (true) {
     // copies all
     auto copy = _master;
@@ -202,18 +194,18 @@ void Server::runServer() {
           // drop client
           close(sock);
           FD_CLR(sock, &_master);
-          removePeer(&userDescriptor, sock);
+          _removePeer(sock);
         } else {
 
           std::ostringstream ss;
           ss << buf;
           std::string s = ss.str();
 
-          findPeerInformation(s, &userDescriptor, sock); 
+          _findPeerInformation(s, sock); 
         }
         // Check if both peers are connected and ready to start a P2P connection
         std::cout << "Sock " << sock << std::endl;
-        readyToP2P(userDescriptor, sock);
+        _readyToP2P(sock);
       }
     }
   }
