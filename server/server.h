a34bf87da673571712c8afb19c3309a53c70c70b
edghyhdz <edgar.hdz@hotmail.com>
2021-07-25 08:56:28 +0200

    Added peerInfo and userInfo struct

--- server/server.h
+++ server/server.h
@@ -10,12 +10,29 @@ Server class definition
 
 #ifndef SERVER_MULT
 #define SERVER_MULT
-
-#define PORT_SERVER 54000
+#define PORT_SERVER 54700
 
 #include <map>
 #include <set>
 
+// Used to keep track of connecting peer info as well as peer to connect to
+struct peerInfo {
+  std::string name;
+  std::string ipAddress;
+  uint16_t port;
+}; 
+
+struct userInfo {
+  bool isClient;
+  bool canConnect{false};   
+  std::string name;
+  std::string ipAddress;
+  uint16_t port;
+  struct peerInfo peerInfo; 
+  int socket;
+};
+
+// Server class declaration
 class Server {
 public:
   Server();
@@ -26,6 +43,7 @@ public:
 private:
   int _listening;
   fd_set _master;
+  std::map<std::string, userInfo> _peerToPeer; 
 };
 
 #endif
