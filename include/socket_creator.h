6d8e5861e467e6169d3ceb8b2e5aea8c5e1e84ea
edghyhdz <edgar.hdz@hotmail.com>
2021-07-25 09:04:05 +0200

    Added peerPort and peerIpAddress private member attributes

--- include/socket_creator.h
+++ include/socket_creator.h
@@ -15,7 +15,7 @@ public:
   Socket(char *&ipAddress, char *&portNum, std::string userName, std::string peerName);
   ~Socket(); 
   void bindToPort();
-  void connectToPeer(std::uint16_t const peerPort, std::string const peerIp);
+  void connectToPeer();
   void close();
   std::string ipAddress() const { return _myIpAddress; }
   std::uint16_t port() const { return _myPort; }
@@ -25,18 +25,21 @@ private:
   struct sockaddr_in _myAddr, _peerAddr;
   void _setIpAddress();
   void _setPort(); 
+  void _bindToPort(); 
   void _connectToServer();
   void _listenToServer(); 
-  void _connectToPeer(std::uint16_t const peerPort, std::string const peerIp);
   
   std::string _userName; 
   int _sockFD; 
+  int _connFD; 
   int _addrInfo; 
   std::string _myIpAddress;
   std::uint16_t _myPort;
   bool _connectionOpen;
   std::thread _t; 
   std::string _peerName; 
+  std::uint16_t _peerPort; 
+  std::string _peerIpAddress; 
 };
 
 #endif
