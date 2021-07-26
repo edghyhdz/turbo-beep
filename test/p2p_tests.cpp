df07308a1575e3fe0085555a852414afd2b36a66
edghyhdz <edgar.hdz@hotmail.com>
2021-07-26 09:49:41 +0200

    First commit

---
+++ test/p2p_tests.cpp
@@ -0,0 +1,74 @@
+// #include "server.h"
+// #include "socket_creator.h"
+#include "tests.h"
+#include <chrono>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <memory>
+#include <thread>
+
+P2PFixture::P2PFixture() {
+  std::uint16_t serverPort{54002};
+  _server = std::make_shared<Server>(serverPort);
+}
+
+P2PFixture::~P2PFixture() {}
+
+void P2PFixture::SetUp() {
+  _ipAddress = new char[10];
+  _port = new char[6];
+  std::string ipStr = "127.0.0.1";
+  std::string portStr = "54002";
+  strcpy(_ipAddress, ipStr.c_str());
+  strcpy(_port, portStr.c_str());
+  std::string userName{"peerOne"};
+  std::string theirUserName{"peerTwo"};
+
+  this->_socket = new Socket(_ipAddress, _port, userName, theirUserName);
+  this->_peerTwo = new Socket(_ipAddress, _port, theirUserName, userName);
+}
+
+void P2PFixture::TearDown() {
+  _tPeer.join();
+  _tServer.detach();
+  delete[] _ipAddress;
+  delete[] _port;
+}
+
+// Wrapper function to return 1 upon successful connection of both peers to the
+// server
+int P2PFixture::peerWrapper() {
+
+  std::mutex m;
+  std::condition_variable cv;
+  int retval{0};
+
+  std::thread t([&cv, &retval, this]() {
+    _peerTwo->connectToServer();
+    retval = 1;
+    cv.notify_one();
+  });
+  t.detach();
+  {
+    std::unique_lock<std::mutex> lck(m);
+    if (cv.wait_for(lck, std::chrono::seconds(2)) == std::cv_status::timeout) {
+      if (!retval) {
+        return retval;
+      }
+    }
+  }
+  return retval;
+}
+
+TEST_F(P2PFixture, SuccessfulDiscconectFromServerAferBothPeersOnline) {
+
+  _tServer = std::thread(&Server::runServer, _server);
+  _tPeer = std::thread(&Socket::connectToServer, _socket);
+  int retval = this->peerWrapper();
+
+  // If both clients connected to the server successfully
+  // They will both return
+  ASSERT_EQ(retval, 1);
+
+  _server.reset();
+}
---
+++ test/server_tests.cpp
@@ -0,0 +1,72 @@
+#include "tests.h"
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <memory>
+#include <thread>
+
+void ServerFixture::SetUp() {
+  std::string buffer;
+  int sock{1};
+
+  buffer = "127.0.0.1::1234::FirstPeer::SecondPeer";
+  server.findPeerInfo(buffer, sock);
+
+  buffer = "195.0.0.1::4321::SecondPeer::FirstPeer";
+  server.findPeerInfo(buffer, ++sock);
+}
+
+TEST_F(ServerFixture, findPeerInfo_FirstPeer) {
+  auto users = server.testUsers();
+
+  ASSERT_EQ(users.at("FirstPeer").name, "FirstPeer");
+  ASSERT_EQ(users.at("FirstPeer").ipAddress, "127.0.0.1");
+  ASSERT_EQ(users.at("FirstPeer").port, 1234);
+  ASSERT_EQ(users.at("FirstPeer").socket, 1);
+  ASSERT_EQ(users.at("FirstPeer").peerInfo.name, "SecondPeer");
+}
+
+TEST_F(ServerFixture, findPeerInfo_FirstPeer_findPeer_isClient_True) {
+  auto users = server.testUsers();
+  ASSERT_EQ(users.at("FirstPeer").isClient, true);
+}
+
+TEST_F(ServerFixture, findPeerInfo_SecondPeer_findPeer_isClient_False) {
+  auto users = server.testUsers();
+  ASSERT_EQ(users.at("SecondPeer").isClient, false);
+}
+
+TEST_F(ServerFixture, findPeerInfo_SecondPeer) {
+  auto users = server.testUsers();
+
+  ASSERT_EQ(users.at("SecondPeer").name, "SecondPeer");
+  ASSERT_EQ(users.at("SecondPeer").ipAddress, "195.0.0.1");
+  ASSERT_EQ(users.at("SecondPeer").port, 4321);
+  ASSERT_EQ(users.at("SecondPeer").socket, 2);
+  ASSERT_EQ(users.at("SecondPeer").peerInfo.name, "FirstPeer");
+  ASSERT_EQ(users.at("SecondPeer").isClient, false);
+}
+
+TEST_F(ServerFixture, ComparePeerInformation) {
+  auto users = server.testUsers();
+
+  ASSERT_EQ(users.at("FirstPeer").peerInfo.name, users.at("SecondPeer").name);
+  ASSERT_EQ(users.at("FirstPeer").peerInfo.port, users.at("SecondPeer").port);
+  ASSERT_EQ(users.at("FirstPeer").peerInfo.ipAddress,
+            users.at("SecondPeer").ipAddress);
+}
+
+TEST_F(ServerFixture, PeersReadyToConnect) {
+  auto users = server.testUsers();
+  ASSERT_EQ(users.at("FirstPeer").canConnect,
+            users.at("SecondPeer").canConnect);
+}
+
+TEST_F(ServerFixture, RemoveFirstPeerFromPeerInfoMap) {
+  // Remove firstPeer
+  server.removePeer(1);
+  auto users = server.testUsers();
+
+  ASSERT_THROW(users.at("FirstPeer"), std::exception);
+  // Second peer should still be here
+  ASSERT_NO_THROW(users.at("SecondPeer"));
+}
