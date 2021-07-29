1b106d4f3ebaab95859bb75cc85786f0bf34d4c9
edghyhdz <edgar.hdz@hotmail.com>
2021-07-29 06:32:50 +0200

    Name changes and added readHeader() and deserializeMessage() methods+

--- src/messages.cpp
+++ src/messages.cpp
@@ -11,7 +11,7 @@ using namespace turbobeep;
  * @param[in] packet packet object needed to serialize coded_output
  * @param[in] myInfo struct containing user information (port, ip, name)
  */
-void messages::UserInfo::addUserInfo(int *size, payload::packet *packet,
+void messages::ProtoBuf::addUserInfo(int *size, payload::packet *packet,
                                      p2p::myInfo const &myInfo) {
 
   auto *payload = packet->mutable_payload();
@@ -29,6 +29,7 @@ void messages::UserInfo::addUserInfo(int *size, payload::packet *packet,
   peerInfo->set_username(myInfo.userName);
   peerInfo->set_peername(myInfo.peerName);
 
+  // 4-byte "magic number" to help use identify size of the packet
   *size = packet->ByteSize() + 4;
 }
 
@@ -39,9 +40,50 @@ void messages::UserInfo::addUserInfo(int *size, payload::packet *packet,
  * @param[in, out] coded_output serialized object to send to peer
  * @param[in] packet packet object needed to serialize coded_output
  */
-void messages::UserInfo::serializeMessage(output_stream *coded_output,
+void messages::ProtoBuf::serializeMessage(output_stream *coded_output,
                                           payload::packet &packet) {
 
   coded_output->WriteVarint32(packet.ByteSize());
   packet.SerializeToCodedStream(coded_output);
+}
+
+/**
+ * Reads headers from packet
+ * 
+ * @param buffer packet buffer, to be later deserialized by
+ * ProtoBuf::deserializeMessage
+ * @returns the size of the message 
+ */
+uint32g messages::ProtoBuf::readHeader(char *buffer) {
+  uint32g size;
+  array_input_stream ais(buffer, 4);
+  input_stream coded_input(&ais);
+  coded_input.ReadVarint32(&size); // Decode the HDR and get the size
+  // std::cout << "size of payload is " << size << std::endl;
+  return size;
+}
+
+/**
+ * Deserializes protobuf packet
+ * 
+ * @param packet protobuf packet to pass buffer data
+ * @param buffer buffer containing data
+ * @param size message size
+ */
+void messages::ProtoBuf::deserializeMessage(payload::packet *packet, char *buffer, uint32g size){
+  // Assign ArrayInputStream with enough memory
+  array_input_stream ais(buffer, size + 4);
+  input_stream coded_input(&ais);
+  // Read an unsigned integer with Varint encoding, truncating to 32 bits.
+  coded_input.ReadVarint32(&size);
+  // After the message's length is read, PushLimit() is used to prevent the
+  // CodedInputStream from reading beyond that length.Limits are used when
+  // parsing length-delimited embedded messages
+  
+  input_stream::Limit msgLimit = coded_input.PushLimit(size);
+  // De-Serialize
+  packet->ParseFromCodedStream(&coded_input);
+  // Once the embedded message has been parsed, PopLimit() is called to undo the
+  // limit
+  coded_input.PopLimit(msgLimit);
 }
