544b1bec8d28d1b431503f89cf13de3893c900f3
edghyhdz <edgar.hdz@hotmail.com>
2021-08-03 06:12:08 +0200

    added payload message type as argument

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,28 @@ using namespace turbobeep;
 int main(int argc, char **argv) {
   auto &ipAddress = argv[1];
   auto &portNum = argv[2];
-  std::string userName = argv[3]; 
-  std::string theirUserName = argv[4]; 
+  if (argc == 5) {
+    std::string userName = argv[3];
+    std::string theirUserName = argv[4];
+    auto mTypePI = payload::packet_MessageTypes_PEER_INFO;
+    auto socket = p2p::Socket(ipAddress, portNum, userName, theirUserName);
+    // Wait for method to return - waiting for other peer
+    socket.connectToServer(mTypePI);
 
-  auto socket = p2p::Socket(ipAddress, portNum, userName, theirUserName);
+    // After other peer has connected -> start trying to connect to peer
+    socket.connectToPeer();
+    
+  } else if (argc == 6) {
+    std::string flag = argv[3];
+    std::string keyPairPath = argv[4];
+    std::string peerPublicKey = argv[5]; 
+    auto mTypeAdv = payload::packet_MessageTypes_ADVERTISE;
+    auto socket = p2p::Socket(ipAddress, portNum, flag, keyPairPath, peerPublicKey);
 
-  // Wait for method to return - waiting for other peer
-  socket.connectToServer(); 
+    // Wait for method to return - waiting for other peer
+    socket.connectToServer(mTypeAdv);
 
-  // After other peer has connected -> start trying to connect to peer
-  socket.connectToPeer(); 
-
+  } else {
+    throw std::runtime_error("Not enough arguments to start ..."); 
+  }
 }
