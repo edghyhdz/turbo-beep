b19ec464c66ff9764fd500188278aac18ec08827
edghyhdz <edgar.hdz@hotmail.com>
2021-08-07 14:07:36 +0200

    renamed class changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "socket_creator.h"
+#include "peer.h"
 #include <iostream>
 
 using namespace turbobeep; 
@@ -10,23 +10,29 @@ int main(int argc, char **argv) {
     std::string userName = argv[3];
     std::string theirUserName = argv[4];
     auto mTypePI = payload::packet_MessageTypes_PEER_INFO;
-    auto socket = p2p::Socket(ipAddress, portNum, userName, theirUserName);
+    auto peer = p2p::Peer(ipAddress, portNum, userName, theirUserName);
     // Wait for method to return - waiting for other peer
-    socket.connectToServer(mTypePI);
+    peer.connectToServer(mTypePI);
     // After other peer has connected -> start trying to connect to peer
-    socket.connectToPeer();
+    peer.connectToPeer();
+    peer.listenToPeer();
     
   } else if (argc == 6) {
     std::string flag = argv[3];
     std::string keyPairPath = argv[4];
     std::string peerPublicKey = argv[5]; 
     auto mTypeAdv = payload::packet_MessageTypes_ADVERTISE;
-    auto socket = p2p::Socket(ipAddress, portNum, flag, keyPairPath, peerPublicKey);
+    auto peer = p2p::Peer(ipAddress, portNum, flag, keyPairPath, peerPublicKey);
 
     // Wait for method to return - waiting for other peer
-    socket.connectToServer(mTypeAdv);
+    peer.connectToServer(mTypeAdv);
     // After other peer has connected -> start trying to connect to peer
-    socket.connectToPeer();
+    peer.connectToPeer();
+    std::thread tListen(&p2p::Peer::listenToPeer, &peer);
+    std::thread tMsgHandler(&p2p::Peer::peerMessageHandler, &peer);
+
+    tListen.join();
+    tMsgHandler.join();
 
   } else {
     throw std::runtime_error("Not enough arguments to start ..."); 
