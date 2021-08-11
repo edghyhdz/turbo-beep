d4c2279767e7f1c31c784dba8292f4c230281423
edghyhdz <edgar.hdz@hotmail.com>
2021-08-11 19:05:40 +0200

    Added _fileHandler, to deal with loading, decoding and sending of the data - plus sending and receiving methods

--- include/messages.h
+++ include/messages.h
@@ -1,9 +1,10 @@
 #ifndef MESSAGES_H
 #define MESSAGES_H
 
+#include "crypto.h"
+#include "file_handler.h"
 #include "payload.pb.h"
 #include "peer.h"
-#include "crypto.h"
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
@@ -20,32 +21,33 @@ namespace messages {
 
 class MessageHandler : public crypto::RSA {
 public:
-  MessageHandler(){};
-  MessageHandler(std::string keyPair, std::string peerPublicKey)
-      : RSA(keyPair, peerPublicKey){};
-    
+  MessageHandler();
+  MessageHandler(std::string keyPair, std::string peerPublicKey);
+
   static long getTimeStamp(); 
   void addUserInfo(int *size, payload::packet *packet,
                           p2p::myInfo const &myInfo,
                           payload::packet::MessageTypes &mType);
   void serializeMessage(output_stream *coded_output, payload::packet &packet);
-
   uint32g readHeader(char *buffer);
   bool deserializeMessage(payload::packet *packet, char *buffer,
                                  uint32g size);
-  
   bool readBody(int sock, uint32g size, payload::packet *packet);
   void sendMessage(int size, int sock, payload::packet &packet);
   bool receiveMessage(int sock, payload::packet *packet); 
   bool receiveMessage(int sock, std::string *recvMsg);
   bool authenticate(int sock); 
-  bool verify(int sock, std::string &key); 
+  bool verify(int sock, std::string &key);
+
+  bool sendFile(int sock, std::string &filePath);  
+  bool receiveFile(int sock); 
 
   payload::packet setPeerData(int *size, std::string &peerIpAddress,
                               std::uint16_t &peerPort,
                               bool &hasAdvertisedFirst);
 
 private:
+  std::unique_ptr<messages::FileHandler> _fileHandler;
   std::string _publicKey;
   std::string _privateKey;
   std::string _peerPublicKey;
