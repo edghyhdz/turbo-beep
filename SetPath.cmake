c7781da53778146814ba099ea5c36513da800b77
edghyhdz <edgar.hdz@hotmail.com>
2021-08-13 10:53:31 +0200

    Included exe path for sh file

--- SetPath.cmake
+++ SetPath.cmake
@@ -1,2 +1,2 @@
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../include/vars.h.in ${CMAKE_CURRENT_SOURCE_DIR}/../include/vars.h)
-message("PATH TO CERTIFICATES SET")
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/../p2p.sh ${CMAKE_CURRENT_BINARY_DIR}/p2p.sh)
