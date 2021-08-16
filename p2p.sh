6a0e2414995bb461576557b88e7a362f54f50387
edghyhdz <edgar.hdz@hotmail.com>
2021-08-16 08:11:17 +0200

    added options

--- p2p.sh
+++ p2p.sh
@@ -3,12 +3,13 @@
 print_options()
 {
    echo ""
-   echo "Usage: $0 p2p [Options] <server ip address> <server port> <your username or public key> <other peer's username / public key>"
+   echo "Usage: $0 [Options] <server ip address> <server port> <your username or public key> <other peer's username / public key>"
    echo ""
    echo "Options inlcude: "
    echo "   -e: p2p Messaging without encryption. Use your username and other peer's username"
    echo "   -m: p2p Messaging with encrpytion. Provide path to your public key and peer's public key"
-   echo "   -f: p2p file sharing. Provide path to your public key and peer's public key and path to file to share (up to 7mb)"
+   echo "   -f: p2p file sharing (send). Provide path to your public key and peer's public key and path to file to share (up to 7mb)"
+   echo "   -r: p2p file sharing (receive). Provide path to your public key and peer's public key"
    echo ""
    exit 0
 }
@@ -35,16 +36,18 @@ run_script()
         "-e") "${P2P_EXE_PATH}" $2 $3 $4 $5;;
         "-m") "${P2P_EXE_PATH}" $2 $3 "0" $4 $5;;
         "-f") "${P2P_EXE_PATH}" $2 $3 "0" $4 $5 $6;;
+        "-r") "${P2P_EXE_PATH}" $2 $3 "0" $4 $5 "-r";;
     esac
     exit 0
 }
 
-while getopts "e:m:f:" opt
+while getopts "e:m:f:r:" opt
 do
    case "$opt" in
       e ) run_script "-e" $2 $3 $4 $5;;
       m ) run_script "-m" $2 $3 $4 $5;;
       f ) run_script "-f" $2 $3 $4 $5 $6;;
+      r ) run_script "-r" $2 $3 $4 $5;;
       h ) print_options;;
       ? ) print_options;;
    esac
